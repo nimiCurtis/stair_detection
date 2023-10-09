ce049fa3b5106ffab9cfbea6eb3172dd0e613f6b
nimiCurtis <nimicu21@gmail.com>
2023-10-09 20:44:21 +0300

    update

--- src/model/train.py
+++ src/model/train.py
@@ -7,13 +7,20 @@ settings.update({'runs_dir': './stair_detection/experiments/runs'})
 model = YOLO('/home/nimrod/ros2_ws/src/stair_detection_pkg/stair_detection/models/yolov8/yolov8n.pt')
 
 # Train the model using the 'coco128.yaml' dataset for 3 epochs
-results = model.train(data='/home/nimrod/ros2_ws/src/stair_detection_pkg/stair_detection/dataset/Myself.v2i.yolov8/data.yaml',
+results = model.train(data='/home/nimrod/ros2_ws/src/stair_detection_pkg/stair_detection/dataset/single_stair_detection.v3i.yolov8/data.yaml',
                     epochs=50,
-                    batch=24,
+                    batch=14,
                     pretrained=False,
-                    device="cpu",
+                    device="cuda",
+                    amp=False,
                     save=True)
 
+# results = model.train(data='/home/nimrod/ros2_ws/src/stair_detection_pkg/stair_detection/dataset/Myself.v3i.yolov8/data.yaml',
+#                     epochs=50,
+#                     batch=14,
+#                     pretrained=False,
+#                     device="cuda",
+#                     save=True)
 
 # Evaluate the model's performance on the validation set
 results = model.val()
